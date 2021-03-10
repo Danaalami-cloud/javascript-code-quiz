53c6adecc7ed58b772ecb26e10582c289e2cf1fb
Dana Alami <dana.alami18@gmail.com>
2021-03-10 18:34:29 +0200

    added variables to JS file

---
+++ script.js
@@ -0,0 +1,23 @@
+// pseudo code
+// start button
+//   geteventlister - clcik to start function 
+// timer  starts 
+// question appears ?hide 
+// answering question triggers next question. ?loop
+// incorrect answer --> subtract from timer (if statement)
+// game over = all answered or time=0 
+//           = save initials and score. 
+
+var startButton= document.getElementById("start-btn");
+var timeEl= document.getElementById("time-remaining");
+
+let questions =[
+    {
+        question:"Which of the following is not JavaScript Data Types?",
+        optionA: "number",
+        optionB: "float",
+        optionC: "undefined",
+        OptionD: "boolean",
+    
+    }
+]
