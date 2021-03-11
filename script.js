42743c0aef2792be9c71711a8ef819cd5b9b6c29
Dana Alami <dana.alami18@gmail.com>
2021-03-11 21:22:48 +0200

    Hiding start screen on click

--- script.js
+++ script.js
@@ -7,23 +7,47 @@
 // incorrect answer --> subtract from timer (if statement)
 // game over = all answered or time=0 
 //           = save initials and score. 
+var startScreen = document.getElementById("start-screen")
+var startButton = document.getElementById("start-btn");
+var timeEl = document.getElementById("time-remaining");
+var score = 0;
+let questions = [
+    {
+        question: "Which of the following is not JavaScript Data Types?",
+        answers: ["A.number", "B.float", "C.undefined", "D.boolean"],
+        correctAnswer: "B.float",
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: ["head", "body", "script", "shoulders"],
+        correctAnswer: "script",
+    },
+    {
+        question: "What is the original name of JavaScript?",
+        answers: ["Mocha", "JavaScript", "EScript", "LiveScript"],
+        correctAnswer: "Mocha",
 
-var startButton= document.getElementById("start-btn");
-var timeEl= document.getElementById("time-remaining");
 
-let questions =[
-        q1={
-            question:"Which of the following is not JavaScript Data Types?",
-        answers: ["number", "float", "undefined","boolean"],
-        correctAnswer:"float",
     }
 ]
 
-for (var i=0; i<questions.length; i++){
-    console.log(questions[i].question)
-    questionText.textContent=(questions[i].question);
-    aEl.textContent=(questions[i].answerA);
-    bEl.textContent=(questions[i].answerB);
-    cEl.textContent=(questions[i].answerC);
-    dEl.textContent=(questions[i].answerD);
+function quizBegin() {
+startScreen.setAttribute("class", "hide")
 }
+
+startButton.onclick = quizBegin
+
+
+
+
+
+
+
+// for (var i = 0; i < questions.length; i++) {
+
+//     questionText.textContent = (questions[i].question);
+//     aEl.textContent = (questions[i].answerA);
+//     bEl.textContent = (questions[i].answerB);
+//     cEl.textContent = (questions[i].answerC);
+//     dEl.textContent = (questions[i].answerD);
+// }
